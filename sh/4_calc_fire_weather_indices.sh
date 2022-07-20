16457bf87151f08b6c41896f2b1012d525bb105e
amyoun01 <amyoun01@gmail.com>
2022-07-19 21:29:26 -0700

    added shell script to calculate fire weather indices.

---
+++ sh/4_calc_fire_weather_indices.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# Initialize workspace 
+# -----------------------------------------------------------------------------
+
+# Activate conda environment
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate pygeo38
+
+# Specify working parent directory and add python scripts to current PATH
+export WDIR=~/projects/boreal_fire_feedbacks
+export PATH="$WDIR/code/finished/py:$PATH"
+
+# List of GCMs
+GCM_LIST=("ACCESS-CM2" "EC-Earth3" "MPI-ESM1-2-HR" "MRI-ESM2-0" "MIROC6")
+
+ERAYR=(1980 2019)
+SIMYR=(2020 2099)
+
+# -----------------------------------------------------------------------------
+# 1. Calculate CFFDRS for ERA5
+# -----------------------------------------------------------------------------
+
+SRC_DIR=$WDIR/processed_data/climate/era5
+DEST_DIR=$WDIR/processed_data/cffdrs/era5
+
+cffdrs_calculate.py -s $SRC_DIR -d $DEST_DIR -t ${ERA_YR[@]}
+
+# -----------------------------------------------------------------------------
+# 2. Calculate CFFDRS for CMIP6
+# -----------------------------------------------------------------------------
+
+for GCM in ${GCM_LIST[@]}; do
+  
+  SRC_DIR=$WDIR/processed_data/climate/cmip6/$GCM/bias_corrected
+  DEST_DIR=$WDIR/processed_data/cffdrs/cmip6/$GCM/bias_corrected
+  
+  cffdrs_calculate.py -s $SRC_DIR -d $DEST_DIR -t ${SIMYR[@]}
+
+done
+
+# Deactivate conda environment
+conda deactivate
